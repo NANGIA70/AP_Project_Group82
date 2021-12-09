a278d9826d90847ea7128b059d995c286e305d4a
Abhimanyu20273 <abhimanyu20273@iiitd.ac.in>
2021-12-09 09:32:38 +0530

    First commit of project

---
+++ src/sample/Controller.java
@@ -0,0 +1,69 @@
+package sample;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+public class Controller implements Initializable  {
+    private int count;
+    @FXML
+    private ImageView image1;
+    @FXML
+    private ImageView image3;
+    @FXML
+    private ImageView image2;
+    @FXML
+    private Group gr1;
+    public static TranslateTransition translate_an_object(Node obj, double x_cord, double y_cord, int duration_set) {
+        Duration animation_time = Duration.millis(duration_set);
+        TranslateTransition translate_object = new TranslateTransition();
+        translate_object.setDuration(animation_time);
+        translate_object.setNode(obj);
+        translate_object.setByX(x_cord);
+        translate_object.setByY(y_cord);
+        return translate_object;
+    }
+    public  void move_object()
+    {
+        final Timeline timeline = new Timeline();
+        Duration animation_time = Duration.millis(2000);
+        TranslateTransition translate_object = new TranslateTransition();
+        translate_object.setDuration(animation_time);
+        translate_object.setNode(gr1);
+        translate_object.setByX(0);
+        translate_object.setByY(100);
+        translate_object.setAutoReverse(true);
+        translate_object.setCycleCount(Timeline.INDEFINITE);
+        translate_object.play();
+    }
+    public  void move_object1()
+    {
+        Duration animation_time = Duration.millis(2000);
+        TranslateTransition translate_object = new TranslateTransition();
+        translate_object.setDuration(animation_time);
+        translate_object.setNode(gr1);
+        translate_object.setByX(100000);
+        translate_object.setByY(0);
+        translate_object.play();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1), e ->
+        {
+            translate_an_object(image1, 20000, 0, 2000).play();
+        }));
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.animation.TranslateTransition;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Main  extends Application {
+    private int count;
+    @FXML
+    private ImageView image1;
+    @FXML
+    private ImageView image3;
+    @FXML
+    private ImageView image2;
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setScene(new Scene(root, 1500, 1000));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public static TranslateTransition translate_an_object(Node obj, double x_cord, double y_cord, int duration_set) {
+        Duration animation_time = Duration.millis(duration_set);
+        TranslateTransition translate_object = new TranslateTransition();
+        translate_object.setDuration(animation_time);
+        translate_object.setNode(obj);
+        translate_object.setByX(x_cord);
+        translate_object.setByY(y_cord);
+        return translate_object;
+    }
+
+}
