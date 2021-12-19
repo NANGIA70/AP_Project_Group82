073709ea9f37d928d4d8f51c971d9a5c447bc453
Abhimanyu20273 <abhimanyu20273@iiitd.ac.in>
2021-12-19 22:56:26 +0530

    Added various functionalities like extending it to full map, better collision and scoreboard improvement

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,11 @@
 package sample;
 
 import javafx.animation.TranslateTransition;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -16,12 +14,10 @@ import javafx.util.Duration;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.ResourceBundle;
 
-import javafx.util.Duration;
-import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 
 public class Controller implements Initializable {
@@ -31,7 +27,7 @@ public class Controller implements Initializable {
 
     private int count = 0;
     private int coinCount = 0;
-
+    private ArrayList<Node> islands = new ArrayList<>();
     @FXML
     private AnchorPane content;
 
@@ -47,6 +43,54 @@ public class Controller implements Initializable {
     private ImageView island2;
     @FXML
     private ImageView island3;
+    @FXML
+    private ImageView island4;
+    @FXML
+    private ImageView island5;
+    @FXML
+    private ImageView island6;
+    @FXML
+    private ImageView island7;
+    @FXML
+    private ImageView island8;
+    @FXML
+    private ImageView island9;
+    @FXML
+    private ImageView island10;
+    @FXML
+    private ImageView island11;
+    @FXML
+    private ImageView island12;
+    @FXML
+    private ImageView island13;
+    @FXML
+    private ImageView island14;
+    @FXML
+    private ImageView island15;
+    @FXML
+    private ImageView island16;
+    @FXML
+    private ImageView island17;
+    @FXML
+    private ImageView island18;
+    @FXML
+    private ImageView island19;
+    @FXML
+    private ImageView island20;
+    @FXML
+    private ImageView island21;
+    @FXML
+    private ImageView island22;
+    @FXML
+    private ImageView island23;
+    @FXML
+    private ImageView island24;
+    @FXML
+    private ImageView island25;
+    @FXML
+    private ImageView island26;
+    @FXML
+    private ImageView island27;
     @FXML
     private ImageView hero;
     @FXML
@@ -73,15 +117,18 @@ public class Controller implements Initializable {
     private ImageView exit;
     @FXML
     private ImageView coin;
-
-
+    @FXML
+    private ImageView green_orc;
     @FXML
     private Group group_menu;
-
+    @FXML
+    private Group group_game;
     @FXML
     private Label distance;
     @FXML
     private Label coin_count;
+    private TranslateTransition hero_up_down_translate_object;
+    private boolean move_click_hero_in_use = false;
 
 //    Function to create required translate transition object
     public static TranslateTransition translate_an_object(Node obj, double x_cord, double y_cord, int duration_set) {
@@ -93,14 +140,18 @@ public class Controller implements Initializable {
         translate_object.setByY(y_cord);
         return translate_object;
     }
-
 //    Function to start indefinite hero jumps
     public  void move_hero() {
         final Timeline timeline = new Timeline();
         TranslateTransition translate_object = translate_an_object(hero, 0, -100, 1000);
+        hero_up_down_translate_object = translate_object;
         translate_object.setAutoReverse(true);
         translate_object.setCycleCount(Timeline.INDEFINITE);
         translate_object.play();
+    }
+    public void stop_hero()
+    {
+        hero_up_down_translate_object.stop();
     }
 
 //    Function to open menu
@@ -144,41 +195,91 @@ public class Controller implements Initializable {
         translate_object.setCycleCount(Timeline.INDEFINITE);
         translate_object.play();
     }
-
 //    Function to close the game
     public  void exit_game() {
         javafx.application.Platform.exit();
     }
-
-//    Function to move hero on mouse click
-    public void move_ClickHero() {
-        if(flag && !pause_flag) {
-//            Move hero forward
-            TranslateTransition translate_object = translate_an_object(hero, 100, 0, 500);
-            translate_object.play();
-
-//            Update distance
-            count+=1;
-            distance.setText(String.valueOf(count));
-
-//            Open Chest
-            if(hero.getBoundsInParent().intersects(chest.getBoundsInParent()) && !chest_open) {
-//               Update chest status
-                chest_open = true;
-
+    public boolean check_island_collision()
+    {
+        for(int i = 0;i<27;i++)
+        {
+            if(hero.getBoundsInParent().intersects(group_game.localToParent(islands.get(i).getBoundsInParent())))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void check_collision()
+    {
+        if(hero.getBoundsInParent().intersects(group_game.localToParent(chest.getBoundsInParent())) && !chest_open) {
+//           Update chest status
+            chest_open = true;
 //                Update coinCount(Add coins)
-                Random rand = new Random();
-                coinCount += (1 + rand.nextInt(6));
-                coin_count.setText(String.valueOf(coinCount));
+            Random rand = new Random();
+            coinCount += (1 + rand.nextInt(6));
+            coin_count.setText(String.valueOf(coinCount));
 
 
 //                Change chest image
-                File file = new File("src/sample/ChestOpen.png");
-                chest.setImage(new Image(file.toURI().toString()));
-            }
+            File file = new File("src/sample/ChestOpen.png");
+            chest.setImage(new Image(file.toURI().toString()));
+        }
+        if(hero.getBoundsInParent().intersects(group_game.localToParent(island3.getBoundsInParent())) || hero.getBoundsInParent().intersects(group_game.localToParent(island2.getBoundsInParent())))
+        {
+            //stop_hero();
+        }
+        if(check_island_collision())
+        {
+            //stop_hero();
+        }
+        if(hero.getBoundsInParent().intersects(group_game.localToParent(green_orc.getBoundsInParent())))
+        {
+            TranslateTransition translate_object = translate_an_object(green_orc, 250, 0, 500);
+            translate_object.play();
+            translate_object.setOnFinished(e -> move_orc_down());
         }
     }
+    public void move_orc_down()
+    {
+       TranslateTransition translate_object = translate_an_object(green_orc, 0, 400, 2000);
+       translate_object.play();
+   }
+//    Function to move hero on mouse click
+    public void move_hero_small(int number)
+    {
+        if(number < 1)
+        {
+            move_click_hero_in_use = false;
+            return;
+        }
+        else
+        {
+            check_collision();
+            TranslateTransition translate_object1 = translate_an_object(group_game, -1,0 , 5);
+            translate_object1.setOnFinished(e -> move_hero_small(number -1));
+            translate_object1.play();
+        }
 
+    }
+    public void move_ClickHero()
+    {
+        if(flag && !pause_flag && !move_click_hero_in_use)
+        {
+            //            Update distance
+            count+=1;
+            distance.setText(String.valueOf(count));
+//            Move hero forward
+            move_click_hero_in_use = true;
+            TranslateTransition translate_object = translate_an_object(orc, -100, 0, 500);
+            translate_object.play();
+            TranslateTransition translate_object1 = translate_an_object(group_game, -1,0 , 5);
+            translate_object1.setOnFinished(e -> move_hero_small(100));
+            translate_object1.play();
+            check_collision();
+        }
+
+    }
 //    Function to start the game
     public void startPlay() {
 //        Update marker
@@ -210,8 +311,19 @@ public class Controller implements Initializable {
 //        Start moving island3 and chest
         move_island(island3);
         move_island(chest);
+       // add_to_group();
     }
+    public void add_to_group()
+    {
+        File file = new File("src/sample/islands7.png");
+        Image image = new Image(file.toURI().toString());
+        ImageView imageview = new ImageView(image);
+        imageview.setLayoutX(495);
+        imageview.setLayoutY(385);
+        imageview.setFitHeight(150);
+        imageview.setFitWidth(200);
 
+    }
     public void move_pause_button_down() {
         TranslateTransition translate_object = translate_an_object(pause, 0, 90, 10);
         translate_object.play();
@@ -237,5 +349,33 @@ public class Controller implements Initializable {
 //            translate_an_object(gr1, 400, 200, 1500).play();
 //        }));
 //        timeline.play();
+        islands.add(island1);
+        islands.add(island2);
+        islands.add(island3);
+        islands.add(island4);
+        islands.add(island5);
+        islands.add(island6);
+        islands.add(island7);
+        islands.add(island8);
+        islands.add(island9);
+        islands.add(island10);
+        islands.add(island11);
+        islands.add(island12);
+        islands.add(island13);
+        islands.add(island14);
+        islands.add(island15);
+        islands.add(island16);
+        islands.add(island17);
+        islands.add(island18);
+        islands.add(island19);
+        islands.add(island20);
+        islands.add(island21);
+        islands.add(island22);
+        islands.add(island23);
+        islands.add(island24);
+        islands.add(island25);
+        islands.add(island26);
+        islands.add(island27);
     }
+
 }
