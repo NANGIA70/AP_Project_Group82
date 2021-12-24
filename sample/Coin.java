4c20dd02fe5fb6bced25ea232428dd6990eec86e
NANGIA70 <76655781+NANGIA70@users.noreply.github.com>
2021-12-24 21:14:30 +0530

    Work before 24th

---
+++ sample/Coin.java
@@ -0,0 +1,43 @@
+package sample;
+
+import javafx.scene.Group;
+import javafx.scene.image.ImageView;
+
+public class Coin extends GameObject{
+    private int coinCount;
+
+    public Coin(/*float x_Coordinate, float y_Coordinate, float x_Speed, float y_Speed,*/ int coinCount, ImageView coin) {
+//        super(x_Coordinate, y_Coordinate, x_Speed, y_Speed);
+        super(coin);
+        this.coinCount = coinCount;
+    }
+
+    public void useCoins() {
+        this.coinCount -= 5;
+    }
+
+    public void addCoins(int coinNum) {
+        coinCount += coinNum;
+    }
+
+//    Getters and Setters
+    public int getCoinCount() {
+        return coinCount;
+    }
+
+    public void setCoinCount(int coinCount) {
+        this.coinCount = coinCount;
+    }
+
+//    Interface Methods
+    @Override
+    public boolean collision(GameObject obj, Group group_game) {
+//        Implement collision with player
+        return false;
+    }
+
+    @Override
+    public void move() {
+//        empty
+    }
+}
---
+++ sample/TreasureChest.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.scene.Group;
+import javafx.scene.image.ImageView;
+
+public abstract class TreasureChest extends GameObject{
+    public TreasureChest(ImageView chest) {
+        super(chest);
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
